853c7848d7e15177721d8df2e2811a2230e3c37f
enricomeloni <enrico-meloni@hotmail.it>
2020-11-23 14:26:44 +0100

    Update powershell script
    Sync to new call to blender script

--- convert.ps1
+++ convert.ps1
@@ -1,3 +1,13 @@
 param ($fbxPath, $objPath, $texPath)
 
-blender --background .\blender\workspace.blend --python .\blender\scripts\convert_fbx_to_obj.py -- $fbxPath $objPath $texPath
+
+if($texPath -is [array])
+{
+    $texPath = $texPath -join " "
+}
+
+$command = "blender --background .\blender\workspace.blend --python .\blender\scripts\convert_fbx_to_obj.py -- $fbxPath $objPath $texPath"
+Write-Host EXECUTING: $command -ForegroundColor White -BackgroundColor Cyan
+Write-Host `n`n 
+
+iex $command
