cb059b079a940513b5a907554a234a3af5a35d83
enricomeloni <enrico-meloni@hotmail.it>
2021-02-26 17:07:47 +0100

    Remove cv2 fix
    SAILenv python api does not use opencv anymore

--- saifooler/render/sailenv_module.py
+++ saifooler/render/sailenv_module.py
@@ -59,7 +59,7 @@ class SailenvModule(pl.LightningModule):
 
         # sailenv return bgr (because opencv) so we need to permute channels
         # we also need to divide by 255 because opencv returns it on range 0..255
-        frame = frame[:, :, [2, 1, 0]] / 255.
+        #frame = frame[:, :, [2, 1, 0]] / 255.
 
         return torch.fliplr(frame).clone().unsqueeze(0).to(self.device)
 
